71097e912052704d9d4b953623a1a0d38cac9f34
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-07-10 23:09:45 -0400

    .replace() , *insert* arrays
    Changed the replace method to reference all instances of the replaced string, and changed the arrays to be enclosed by square brackets instead of parentheses.

--- lab4/part1/js/main.js
+++ lab4/part1/js/main.js
@@ -13,20 +13,20 @@ function randomValueFromArray(array){
 
 storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-insertX = ("Willy the Goblin",
+insertX = ["Willy the Goblin",
           "Big Daddy",
           "Father Christmas" 
-          );
+          ];
 
-insertY = ("the soup kitchen",
+insertY = ["the soup kitchen",
            "Disneyland",
            "the White House"
-          );
+          ];
 
-insertZ = ("spontaneously combusted",
+insertZ = ["spontaneously combusted",
            "melted into a puddle on the sidewalk",
            "turned into a slug and crawled away"
-);
+          ];
 
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
@@ -38,22 +38,23 @@ function result() {
   xItem = randomValueFromArray(insertX);
   yItem = randomValueFromArray(insertY);
   zItem = randomValueFromArray(insertZ);
-  newStory = newStory.replace(":insertx:", xItem);
-  newStory = newStory.replace(":inserty:", yItem);
-  newStory = newStory.replace(":insertz:", zItem);
+  newStory = newStory.replace(/:insertx:/g, xItem);
+  newStory = newStory.replace(/:inserty:/g, yItem);
+  newStory = newStory.replace(/:insertz:/g, zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("Bob", name);
+    newStory = newStory.replace(/Bob/g, name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300 / 14);
-    const temperature =  Math.round(94) + ' stone';
-
+    const weight = Math.round(300 / 14) + ' stone';
+    const temperature =  Math.round((94 - 32) * (5/9)) + ' centigrade';
+    newStory = newStory.replace(/300 pounds/g, weight);
+    newStory = newStory.replace(/94 fahrenheit/g, temperature);
   }
 
-  story.textContent = ; // To correct - Completing the result() function: - https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Silly_story_generator
+  story.textContent = newStory; 
   story.style.visibility = 'visible';
 }

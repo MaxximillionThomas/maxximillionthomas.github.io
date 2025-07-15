05bb044053f242f62417af6cfb60348738a58a40
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-07-15 10:59:33 -0400

    Pictures arrays + iteration
    Added constant arrays for the image names and their respective alt text, then created a for loop for creating an img tag that uses the paired information as attributes based on array indexing.

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -5,10 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const PICTURES = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const PICTURES_ALT = ["eyeball", "maybe a seashell", "flowers", "false gods", "niagara"];
 
 /* Looping through images */
+for (let i = 0; i < PICTURES.length; i++) {
+    let img_path = "../images/" + PICTURES[i];
+    img = document.createElement("img");
+    img.setAttribute("src", img_path);
+    img.setAttribute("alt", PICTURES_ALT[i]);
+    thumbBar.appendChild(img);
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);

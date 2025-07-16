2d576832595343b933acf395cd40719f5217376a
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-07-16 15:10:31 -0400

    Updated js to include comments
    As the new file was downloaded to ensure integrity of code, applicable comments from lab 3's draft version were carried over.
    Balls currently not showing after Shape extension to Ball.

--- lab4/part4/js/main-finished.js
+++ lab4/part4/js/main-finished.js
@@ -1,4 +1,4 @@
-// set up canvas
+// setup canvas
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -12,7 +12,7 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
+// function to generate random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -25,17 +25,20 @@ class Shape {
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    exists = true;
   }
 }
 
 // ###############    BALL    ###############
 class Ball extends Shape {
+  // Instantiate the bouncy ball object
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
     this.color = color;
     this.size = size;
   }
 
+  // Draw the ball onto the screen 
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -43,7 +46,9 @@ class Ball extends Shape {
     ctx.fill();
   }
 
+  // Update the ball's position (& velocity after it hits window boundaries)
   update() {
+    // Check position relative to left/right boundaries
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
     }
@@ -52,6 +57,7 @@ class Ball extends Shape {
       this.velX = Math.abs(this.velX);
     }
 
+    // Check position relative to top/bottom boundaries
     if (this.y + this.size >= height) {
       this.velY = -Math.abs(this.velY);
     }
@@ -60,13 +66,15 @@ class Ball extends Shape {
       this.velY = Math.abs(this.velY);
     }
 
+    // Update position 
     this.x += this.velX;
     this.y += this.velY;
   }
 
+  // Change ball colors when they make contact
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -79,12 +87,13 @@ class Ball extends Shape {
   }
 }
 
+// Create new array of balls
 const balls = [];
-
+// Randomize 25 unique balls and add them to the array
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
+    // Ball position always drawn at least one ball width
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
@@ -93,21 +102,25 @@ while (balls.length < 25) {
     randomRGB(),
     size
   );
-
+  // Add the ball to the array
   balls.push(ball);
 }
 
+// Create a loop to automatically draw the canvas and it's balls, refreshing with each update
 function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  // Draw the canvas
+  ctx.fillStyle = "rgba(0, 0, 0, 0.37)";  
   ctx.fillRect(0, 0, width, height);
-
+  // Draw the balls
   for (const ball of balls) {
     ball.draw();
     ball.update();
     ball.collisionDetect();
   }
-
+  // Refresh the canvas
   requestAnimationFrame(loop);
 }
 
+// Call the canvas and balls for visualization
 loop();
+

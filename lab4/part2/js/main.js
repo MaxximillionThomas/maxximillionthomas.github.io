96a0ea1eca9d32aa9802c695e4c7bc9ff0808e94
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-07-23 20:43:48 -0400

    Image disctionary
    Image paths and alt text have been stored to a dictionary in place of the previous 2 independent arrays, per professor request.

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -5,36 +5,28 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-// =============== Arrays ===============
-// Array of image filenames */
-const PICTURES = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
-// Array of alternative text for each image file */
-const PICTURES_ALT = ["eyeball", "maybe a seashell", "flowers", "false gods", "niagara"];
+// =============== Images ===============
+// Dictionary of image names and alternate text
+const IMAGE_PAIRS = {"pic1.jpg": "eyeball", "pic2.jpg": "maybe a seashell", 
+                    "pic3.jpg": "flowers", "pic4.jpg": "false gods", "pic5.jpg": "niagara"};
 
 // =============== Gallery Image Swapping ===============
 // Enable swapping of gallery image 
 function displayImage(event) {
-    // Assign an identity to the clicked image
-    const TARGET_IMAGE = event.target;
-    // Store attributes
-    img_path = TARGET_IMAGE.getAttribute("src");
-    alt_text = TARGET_IMAGE.getAttribute("alt");
     // Assign attributes to the display image
-    displayedImage.setAttribute("src", img_path);
-    displayedImage.setAttribute("alt", alt_text);
+    displayedImage.setAttribute("src", this.src); 
+    displayedImage.setAttribute("alt", this.alt);
 }
 
 // =============== New img Elements ===============
 // Iterate through each src-alt pairing to create img elements
-/* !!! NOTE for professor!!!
-   Rubric asks for index iteration starting at 1, but doing so would
-   misalign the dual-array indexing in a clunky and unprofessional way. */ 
-for (let i = 0; i < PICTURES.length; i++) {
+for (let i = 1; i < Object.keys(IMAGE_PAIRS).length + 1; i++) {
     // Create a new img element
     let img = document.createElement("img");
     // Establish attributes
-    let img_path = "images/" + PICTURES[i];
-    let alt_text = PICTURES_ALT[i];
+    let key = "pic" + i + ".jpg";
+    let img_path = "images/" + key;      
+    let alt_text = IMAGE_PAIRS[key];
     // Assign attributes to the img element
     img.setAttribute("src", img_path);
     img.setAttribute("alt", alt_text);
@@ -45,14 +37,14 @@ for (let i = 0; i < PICTURES.length; i++) {
 }
 
 // Determine the brightness of the image
-function darkenLighten(elem) {
-    if (elem.getAttribute("class") == "dark") {
-        elem.setAttribute("class", "light")
+function darkenLighten() {
+    if (btn.getAttribute("class") == "dark") {
+        btn.setAttribute("class", "light")
         overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
         btn.textContent = "Lighten";
         btn.style.background = "rgba(223, 220, 220, 0.6)"
     } else {
-        elem.setAttribute("class", "dark")
+        btn.setAttribute("class", "dark")
         overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
         btn.textContent = "Darken";
         btn.style.background = "rgba(150, 150, 150, 0.6)"
@@ -60,8 +52,7 @@ function darkenLighten(elem) {
 }
 
 // Add button functionality for switching modes
-btn.addEventListener("click", function() {
-    darkenLighten(this);
-});
+btn.addEventListener("click", darkenLighten);
+
 
 
